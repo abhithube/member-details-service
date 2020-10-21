ec945f7aaca87d1bd28e8120047373b5521e0bd1
Abhi Thube <abhimanyuthube@gmail.com>
2020-10-20 22:24:50 -0700

    Testing deploy script #3

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-$(docker run --rm -it amazon/aws-cli ecr get-login-password --region us-west-2)
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+$(aws ecr get-login-password --region us-west-2)
 docker tag insurance-portal/member-details:latest 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/member-details:latest
 docker push 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/member-details:latest
