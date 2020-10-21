5d182b82afb229c89f637908a351c007d8f08bda
Abhi Thube <abhimanyuthube@gmail.com>
2020-10-20 22:09:01 -0700

    Testing deploy script #1

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -it amazon/aws-cli ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 449094484520.dkr.ecr.us-west-2.amazonaws.com
+$(aws ecr get-login --no-include-email --region us-west-2)
 docker tag insurance-portal/member-details:latest 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/member-details:latest
 docker push 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/member-details:latest
