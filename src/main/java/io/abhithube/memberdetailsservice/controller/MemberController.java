a328928fb4af8f942577b9aa3eadb34a6fd5c859
Abhi Thube <abhimanyuthube@gmail.com>
2020-10-23 20:48:56 -0700

    Added support for API versioning, and removed CORS config

--- src/main/java/io/abhithube/memberdetailsservice/controller/MemberController.java
+++ src/main/java/io/abhithube/memberdetailsservice/controller/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/v1/members")
 public class MemberController {
     private final MemberService memberService;
 
